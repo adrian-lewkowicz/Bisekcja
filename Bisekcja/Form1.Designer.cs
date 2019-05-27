3344cb2697c147dfbe9579c013e1e5b7e4c60d87
Adrian Lewkowicz <adrian.lewkowicz.biala@gmail.com>
2019-05-27 21:59:12 +0200

    Dodaj pliki projektów.

---
+++ Bisekcja/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Bisekcja
+{
+    partial class Bisekcja
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPow4 = new System.Windows.Forms.TextBox();
+            this.textBoxPow1 = new System.Windows.Forms.TextBox();
+            this.textBoxPow2 = new System.Windows.Forms.TextBox();
+            this.textBoxPow3 = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPeriod = new System.Windows.Forms.TextBox();
+            this.textBoxEpsilon = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPow4
+            // 
+            this.textBoxPow4.Location = new System.Drawing.Point(85, 141);
+            this.textBoxPow4.Name = "textBoxPow4";
+            this.textBoxPow4.Size = new System.Drawing.Size(23, 22);
+            this.textBoxPow4.TabIndex = 0;
+            // 
+            // textBoxPow1
+            // 
+            this.textBoxPow1.Location = new System.Drawing.Point(301, 144);
+            this.textBoxPow1.Name = "textBoxPow1";
+            this.textBoxPow1.Size = new System.Drawing.Size(23, 22);
+            this.textBoxPow1.TabIndex = 1;
+            // 
+            // textBoxPow2
+            // 
+            this.textBoxPow2.Location = new System.Drawing.Point(229, 143);
+            this.textBoxPow2.Name = "textBoxPow2";
+            this.textBoxPow2.Size = new System.Drawing.Size(23, 22);
+            this.textBoxPow2.TabIndex = 2;
+            // 
+            // textBoxPow3
+            // 
+            this.textBoxPow3.Location = new System.Drawing.Point(157, 141);
+            this.textBoxPow3.Name = "textBoxPow3";
+            this.textBoxPow3.Size = new System.Drawing.Size(23, 22);
+            this.textBoxPow3.TabIndex = 3;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(357, 144);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(23, 22);
+            this.textBoxC.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "x^4+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "x^3+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "x^2+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(329, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "x+";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(386, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "=0";
+            // 
+            // textBoxPeriod
+            // 
+            this.textBoxPeriod.Location = new System.Drawing.Point(301, 52);
+            this.textBoxPeriod.Name = "textBoxPeriod";
+            this.textBoxPeriod.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPeriod.TabIndex = 10;
+            // 
+            // textBoxEpsilon
+            // 
+            this.textBoxEpsilon.Location = new System.Drawing.Point(301, 92);
+            this.textBoxEpsilon.Name = "textBoxEpsilon";
+            this.textBoxEpsilon.Size = new System.Drawing.Size(100, 22);
+            this.textBoxEpsilon.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Podaj przedział rozdzielony znakiem ,";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Podaj epsilon";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(229, 194);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 48);
+            this.buttonStart.TabIndex = 14;
+            this.buttonStart.Text = "Oblicz";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(569, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // Bisekcja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxEpsilon);
+            this.Controls.Add(this.textBoxPeriod);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxPow3);
+            this.Controls.Add(this.textBoxPow2);
+            this.Controls.Add(this.textBoxPow1);
+            this.Controls.Add(this.textBoxPow4);
+            this.Name = "Bisekcja";
+            this.Text = "Bisekcja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPow4;
+        private System.Windows.Forms.TextBox textBoxPow1;
+        private System.Windows.Forms.TextBox textBoxPow2;
+        private System.Windows.Forms.TextBox textBoxPow3;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPeriod;
+        private System.Windows.Forms.TextBox textBoxEpsilon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Bisekcja/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Bisekcja
+{
+    public partial class Bisekcja : Form
+    {
+        public Bisekcja()
+        {
+            InitializeComponent();
+        }
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+            string[] tab = textBoxPeriod.Text.Split(',');
+            double periodStart = Convert.ToDouble(tab[0]);
+            double periodEnd = Convert.ToDouble(tab[1]);
+            double epsilon = Convert.ToDouble(textBoxEpsilon.Text);
+            double pomoc;
+            if (Licz_Funkc(periodStart)* Licz_Funkc(periodEnd)<0)
+            {
+                do
+                {
+                    pomoc = ((periodStart + periodEnd) / 2);
+                    if (Licz_Funkc(periodStart) > 0 && Licz_Funkc(pomoc) < 0)
+                    {
+                        periodEnd = pomoc;
+                    }
+                    if (Licz_Funkc(periodEnd) > 0 && Licz_Funkc(pomoc) < 0)
+                    {
+                        periodStart = pomoc;
+                    }
+                    if (Licz_Funkc(periodStart) < 0 && Licz_Funkc(pomoc) > 0)
+                    {
+                        periodEnd = pomoc;
+                    }
+                    if (Licz_Funkc(periodEnd) < 0 && Licz_Funkc(pomoc) > 0)
+                    {
+                        periodStart = pomoc;
+                    }
+                    textBox1.Text = pomoc.ToString();
+                } while (Math.Abs(Licz_Funkc(pomoc)) > epsilon);
+                textBox1.Text = pomoc.ToString();
+            }
+            else
+            {
+                textBox1.Text = "błędny przedział";
+            }
+        }
+        public double Licz_Funkc(double x)
+        {
+            double fourPower = Convert.ToDouble(textBoxPow4.Text);
+            double thrrePower = Convert.ToDouble(textBoxPow3.Text);
+            double twoPower = Convert.ToDouble(textBoxPow2.Text);
+            double onePower = Convert.ToDouble(textBoxPow1.Text);
+            double C = Convert.ToDouble(textBoxC.Text);
+            double funkc = 0;
+            funkc = fourPower * Math.Pow(x, 4) + thrrePower * Math.Pow(x, 3) + twoPower * Math.Pow(x, 2) + onePower * Math.Pow(x, 1) + C;
+            return funkc;
+        }
+    }
+}
